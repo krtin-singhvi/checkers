3def852a52bc8c1397966580547d0173c245ca41
krtin-singhvi <krtin.singhvi06@gmail.com>
2025-11-12 16:47:06 +0530

    some constant values

---
+++ constants.py
@@ -0,0 +1,8 @@
+import pygame
+black = (0,0,0)
+red = (255,0,0)
+gold = (255, 215, 0)
+screen_size = 800
+columns = 8
+square_size = screen_size//columns
+disp = pygame.display.set_mode((screen_size, screen_size))
