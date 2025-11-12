3481c0587236ce0f1d04651dbf09bd1c2c87f7e3
krtin-singhvi <krtin.singhvi06@gmail.com>
2025-11-12 19:48:01 +0530

    added some constants

--- constants.py
+++ constants.py
@@ -2,7 +2,9 @@ import pygame
 black = (0,0,0)
 red = (255,0,0)
 gold = (255, 215, 0)
+light_brown = (245,222,179)
+dark_brown = (139,69,19)
 screen_size = 800
 columns = 8
-square_size = screen_size//columns
+square_size = screen_size//columns 
 disp = pygame.display.set_mode((screen_size, screen_size))
