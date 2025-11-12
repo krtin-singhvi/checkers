4017990074dea2c1c8a643216979fa2e350288f5
krtin-singhvi <krtin.singhvi06@gmail.com>
2025-11-12 16:45:47 +0530

    Piece movement and intialization

---
+++ piece.py
@@ -0,0 +1,28 @@
+import pygame
+from constants import black, red, gold, square_size, screen_size, disp
+class piece():
+    def __init__(self, row, col, color):
+        self.row = row
+        self.col = col
+        self.color = color
+        self.find_coord()
+    
+    def make_king(self):
+        self.king = True
+        self.color = gold
+    
+    def find_coord(self):
+        self.x = int(square_size(self.row - 0.5))
+        self.y = int(square_size(self.col - 0.5))
+    
+    def draw(self):
+        radius = square_size - 10
+        if self.king:
+            pygame.draw.circle(disp, self.color, (self.x, self.y), radius)
+        else:
+            pygame.draw.circle(disp, self.color, (self.x, self.y), radius)
+    
+    def move(self, row, col):
+        self.row = row
+        self.col = col
+        self.find_coord()
