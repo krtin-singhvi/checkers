7c63b64e96138871e5818d4dea1b78e9d4a71a6b
krtin-singhvi <krtin.singhvi06@gmail.com>
2025-11-16 00:20:49 +0530

    Board creation and piece movement

--- board.py
+++ board.py
@@ -1,7 +1,7 @@
 import pygame
-from piece import piece
+from piece import Piece
 from constants import screen_size, square_size, light_brown, dark_brown, columns, disp, red, black
-class board():
+class Board():
     def __init__(self, columns):
         self.board = []
         disp.fill(light_brown)
@@ -12,15 +12,15 @@ class board():
                 if (i + j) % 2 != 0:
                     pygame.draw.rect(disp, dark_brown, (x, y, square_size, square_size))
         self.put_pieces()
-                
-    def put_pieces(self):
+                  
+    def put_pieces(self): 
         for i in range(columns):
             self.board.append([])
             for j in range(columns):
                 if (i + j) % 2 != 0 and i < columns//2-1:
-                    self.board[i].append(piece(i, j, black))
+                    self.board[i].append(Piece(i, j, red))
                 elif (i + j)%2 != 0 and i > columns//2:
-                    self.board[i].append(piece(i, j, red))
+                    self.board[i].append(Piece(i, j, black))
                 else:
                     self.board[i].append(0)
         self.draw_pieces()
@@ -38,3 +38,54 @@ class board():
             self.board[old_row][old_col] = 0
             pygame.draw.rect(disp, dark_brown, (old_col*square_size, old_row*square_size, square_size, square_size))
         self.draw_pieces()
+  
+class Select():
+    def __init__(self, board):
+        self.selected = None
+        self.board = board
+
+    def get_row_col(self, x, y):
+        row = y//square_size
+        col = x//square_size
+        return (row, col)
+    
+    def handle_click(self, x, y):
+        row, col = self.get_row_col(x, y)
+        clicked_square = self.board.board[row][col]
+
+        if self.selected == None:
+            if clicked_square != 0:
+               self.selected = clicked_square
+               Select.highlight(row, col)
+
+
+        elif isinstance(self.selected, Piece):
+            Select.clear(self.selected.row, self.selected.col)
+            if clicked_square == 0 and (row + col)%2 != 0:
+                b.move_piece(self.selected.row, self.selected.col, row, col)
+                self.selected = None
+            elif clicked_square != 0:
+                Select.highlight(clicked_square.row, clicked_square.col)
+                self.selected = clicked_square
+
+
+        elif isinstance(self.selected, tuple):
+            r, c = self.selected
+            Select.clear(r, c)
+            if clicked_square != 0:
+                self.selected = clicked_square
+            else:
+                self.selected = (row,col)
+
+    def new_method(self, row, col):
+        Select.highlight(row, col)             
+
+    def clear(row, col):
+    
+        if (col + row)%2 != 0:
+            pygame.draw.rect(disp, dark_brown, (square_size*col, square_size*row, square_size, square_size), 5)
+        else:
+            pygame.draw.rect(disp, light_brown, (square_size*col, square_size*row, square_size, square_size), 5)
+    
+    def highlight(row, col):
+        pygame.draw.rect(disp, (0,255,0), (square_size*col, square_size*row, square_size, square_size), 5)
