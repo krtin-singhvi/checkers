10f328a9c068bff368e3ef40b4955c05c2675b61
veddsavjani <ved.savjani@iiitb.ac.in>
2025-11-22 14:23:15 +0530

    Refactor Piece class and update draw method
    Refactored Piece class to simplify initialization and drawing.

--- piece.py
+++ piece.py
@@ -1,29 +1,19 @@
 import pygame
-from constants import black, red, gold, square_size, screen_size, disp
-class Piece():
-    def __init__(self, row, col, color):
-        self.row = row
-        self.col = col
+from constants import *
+
+class Piece:
+    def __init__(self, color):
         self.color = color
         self.king = False
-        self.find_coord()
-    
+
     def make_king(self):
         self.king = True
-        self.color = gold
-    
-    def find_coord(self):
-        self.x = int(square_size*(self.col + 0.5))
-        self.y = int(square_size*(self.row + 0.5))
-    
-    def draw(self):
-        radius = square_size//2 - 10
+
+    def draw(self, window, x, y, radius):
+        if self.color == "red":
+            color = RED_COLOR
+        elif self.color == "black":
+            color = BLACK_COLOR
+        pygame.draw.circle(window, color, (x,y), radius)
         if self.king:
-            pygame.draw.circle(disp, self.color, (self.x, self.y), radius)
-        else:
-            pygame.draw.circle(disp, self.color, (self.x, self.y), radius)
-    
-    def move(self, row, col):
-        self.row = row
-        self.col = col
-        self.find_coord()
+            pygame.draw.circle(window, GOLD, (x,y), radius, 3)
