cbb64223baa8cc10f810445eed9b03cf67dd1c91
veddsavjani <ved.savjani@iiitb.ac.in>
2025-11-22 14:23:47 +0530

    Refactor Board class and piece setup logic

--- board.py
+++ board.py
@@ -1,74 +1,59 @@
 import pygame
+from constants import *
 from piece import Piece
-from constants import screen_size, square_size, light_brown, dark_brown, columns, disp, red, black
-class Board():
-    def __init__(self, columns):
-        self.board = []
-        disp.fill(light_brown)
-        for i in range(columns):
-            for j in range(columns):
-                x = j*square_size
-                y = i*square_size
-                if (i + j) % 2 != 0:
-                    pygame.draw.rect(disp, dark_brown, (x, y, square_size, square_size))
-        self.put_pieces()
-                  
-    def put_pieces(self): 
-        for i in range(columns):
-            self.board.append([])
-            for j in range(columns):
-                if (i + j) % 2 != 0 and i < columns//2-1:
-                    self.board[i].append(Piece(i, j, red))
-                elif (i + j)%2 != 0 and i > columns//2:
-                    self.board[i].append(Piece(i, j, black))
+
+class Board:
+    def __init__(self):
+        # create an 8x8 grid filled with None
+        self.grid = []
+        for i in range(ROWS):
+            self.grid.append([])
+            for j in range(COLS):
+                self.grid[i].append(None)
+        
+
+    def setup(self):
+        # place pieces in starting positions on dark squares
+        # top 3 rows = black, bottom 3 rows = red
+        for i in range(ROWS):
+            for j in range(COLS):
+                if (i +j)%2 != 0 and i<ROWS//2 -1:
+                    self.grid[i][j] = Piece("black") 
+                elif (i +j)%2 != 0 and i>ROWS//2:
+                    self.grid[i][j] = Piece("red")    
                 else:
-                    self.board[i].append(0)
-        self.draw_pieces()
+                    self.grid[i][j] = None 
 
-    def draw_pieces(self):
-        for row in self.board:
-            for piece in row:
-                if piece != 0:
-                    piece.draw()
-    
-    def move_piece(self, old_row, old_col, new_row, new_col):
-        if self.board[old_row][old_col] != 0:
-            self.board[old_row][old_col].move(new_row, new_col)
-            self.board[new_row][new_col] = self.board[old_row][old_col]
-            self.board[old_row][old_col] = 0
-            pygame.draw.rect(disp, dark_brown, (old_col*square_size, old_row*square_size, square_size, square_size))
-        self.draw_pieces()
-  
-class Select():
-    def __init__(self, board):
-        self.selected = None
-        self.board = board
+    def draw_squares(self, window):
+        # draw 8x8 alternating light/dark squares
+        for i in range(ROWS):
+            for j in range(COLS):
+                rect = (j * SQUARE_SIZE, i * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
+                if (i + j)%2 !=0:
+                    pygame.draw.rect(window, DARK_COLOR, rect)
+                else:
+                    pygame.draw.rect(window, LIGHT_COLOR, rect)                 
+        
 
-    def get_row_col(self, x, y):
-        row = y//square_size
-        col = x//square_size
-        return (row, col)
-    
-    def handle_click(self, x, y):
-        row, col = self.get_row_col(x, y)
-        clicked_square = self.board.board[row][col]
+    def draw_pieces(self, window):
+        # loop through grid; if there is a piece, tell it to draw itself
+        for i in range(ROWS):
+            for j in range(COLS):
+                piece = self.grid[i][j]
+                if piece is not None:
+                    x = SQUARE_SIZE * (j + 0.5)
+                    y = SQUARE_SIZE * (i + 0.5)
+                    radius =  SQUARE_SIZE // 2 - 10
+                    piece.draw(window, x, y, radius)
+        
 
-        if self.selected == None:
-            if clicked_square != 0:
-               self.selected = clicked_square
-               Select.highlight(row, col)
+    def draw(self, window):
+        self.draw_squares(window)
+        self.draw_pieces(window)
+        
 
-        elif isinstance(self.selected, Piece):
-            Select.clear(self.selected.row, self.selected.col)
-            if clicked_square == 0 and (row + col)%2 != 0:
-                b.move_piece(self.selected.row, self.selected.col, row, col)
-                self.selected = None
-            elif clicked_square != 0:
-                Select.highlight(clicked_square.row, clicked_square.col)
-                self.selected = clicked_square         
-
-    def clear(row, col):
-            pygame.draw.rect(disp, dark_brown, (square_size*col, square_size*row, square_size, square_size), 5)
-    
-    def highlight(row, col):
-        pygame.draw.rect(disp, (0,255,0), (square_size*col, square_size*row, square_size, square_size), 5)
+    def get_square(self, x, y):
+        # convert pixel x,y to board row,col
+        row = y // SQUARE_SIZE
+        col = x // SQUARE_SIZE
+        return row, col
